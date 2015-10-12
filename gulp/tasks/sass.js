a5bb82dc486697ad3634e30f65b7d6e4ca8e6325
Ian Harvey <mail.iharvey@gmail.com>
2015-10-12 16:55:30 +0100

    Reworked CSS process
    Minification moved into sass pipeline
    Comments and docblock now honoured
    Sourcemap fixes

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,20 +1,44 @@
 'use strict';
 
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
-var sass         = require('gulp-ruby-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var handleErrors = require('../util/handleErrors');
-var config       = require('../config').sass;
-var autoprefixer = require('gulp-autoprefixer');
+var gulp          = require('gulp');
+var browserSync   = require('browser-sync');
+var sass          = require('gulp-ruby-sass');
+var sourcemaps    = require('gulp-sourcemaps');
+var autoprefixer  = require('gulp-autoprefixer');
+var minifyCSS     = require('gulp-minify-css');
+var header        = require('gulp-header');
+var handleErrors  = require('../util/handleErrors');
+var config        = require('../config').sass;
+var pkg           = require('../../package.json');
+
+var banner = [
+  '/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @license <%= pkg.license %>',
+  ' * Compiled <%= new Date().toUTCString() %>',
+  ' */',
+  ''
+].join('\n');
 
 gulp.task('sass', function () {
 
   return sass(config.src, config.settings)
+
     .pipe(sourcemaps.init())
     .on('error', handleErrors)
-    .pipe(sourcemaps.write())
     .pipe(autoprefixer())
+    .pipe(minifyCSS({
+      keepSpecialComments: "*",
+      roundingPrecision: -1,
+      restructuring: false,
+      mediaMerging: false,
+      keepBreaks: true,
+      advanced: false
+    }))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 
