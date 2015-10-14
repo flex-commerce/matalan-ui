0f09e4c87363ccdcac974233c3ce32e1b38e11ea
Ian Harvey <mail.iharvey@gmail.com>
2015-10-14 13:47:22 +0100

    Lorem defaults

--- src/hb-helpers/lorem.js
+++ src/hb-helpers/lorem.js
@@ -1,25 +1,30 @@
-/**
- *
- * Usage
- *   {{lorem}}
- *   {{lorem count="7" units="words"}}
- *
- * Default settings
- *   count: 1,               // Number of words, sentences, or paragraphs to generate.
- *   units: 'sentences',     // Generate words, sentences, or paragraphs.
- *   sentenceLowerBound: 5,  // Minimum words per sentence.
- *   sentenceUpperBound: 10, // Maximum words per sentence.
- *   paragraphLowerBound: 3, // Minimum sentences per paragraph.
- *   paragraphUpperBound: 7, // Maximum sentences per paragraph.
- *   format: 'plain',        // Plain text or html
- *   random: Math.random     // A PRNG function. Uses Math.random by default
- *
- */
-
 'use strict';
 
-module.exports.register = function(handlebars) {
+var loremIpsum = require('helper-lorem/node_modules/lorem-ipsum');
+var extend = require('helper-lorem/node_modules/extend-shallow');
 
-  handlebars.registerHelper('lorem', require('helper-lorem'));
+module.exports = function lorem(locals) {
+  var context = extend({}, this && this.context, locals);
 
+  // Handlebars `root`
+  if (context.data && context.data.root) {
+    context = extend({}, context, context.data.root);
+  }
+
+  // Handlebars options `hash`
+  if (context.hash) {
+    context = extend({}, context, context.hash);
+  }
+
+  // default settings
+  return loremIpsum(extend({
+    count: 1,               // Number of words, sentences, or paragraphs to generate.
+    units: 'words',     // Generate words, sentences, or paragraphs.
+    sentenceLowerBound: 5,  // Minimum words per sentence.
+    sentenceUpperBound: 10, // Maximum words per sentence.
+    paragraphLowerBound: 3, // Minimum sentences per paragraph.
+    paragraphUpperBound: 7, // Maximum sentences per paragraph.
+    format: 'plain',        // Plain text or html
+    random: Math.random     // A PRNG function. Uses Math.random by default
+  }, context));
 };
