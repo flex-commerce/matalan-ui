44a80785a1445073ba29d089e906f1cd3cbf86f3
amjgbr <amjgbr@gmail.com>
2015-11-24 10:15:22 +0000

    fixed untracked files

---
+++ aws/23-11-2015/js/filters.js
@@ -0,0 +1,54 @@
+'use strict';
+
+(function() {
+    var filtersControl = $('.js-filters');
+    var filtersClose = $('.o-filters--close');
+    var clickCatcher = '<div data-app="toggle" data-toggle=".filters" id="js-clickcatcher-filters" class="js-clickcatcher"></div>';
+    var filtersActive = false;
+
+  function limitFilters () {
+
+  }
+
+  function closeFilters(scope) {
+    $('html').removeClass("body--modal-open");
+    $(scope).closest('.container').removeClass("open");
+    $('#js-clickcatcher-filters').remove();
+
+    filtersActive = false;
+  }
+
+  function openFilters(scope) {
+    $('html').addClass("body--modal-open");
+    $(scope).closest('.container').addClass("open");
+    $('body').append(clickCatcher);
+    filtersActive = true;
+  }
+
+  $('body').on('click', '#js-clickcatcher-filters', function() {
+    closeFilters(filtersControl);
+  });
+
+  filtersControl.on('click', function() {
+    if (filtersActive === true) {
+      closeFilters(filtersControl);
+    } else {
+      openFilters(filtersControl);
+    }
+  });
+
+  filtersClose.on('click', function() {
+    closeFilters(filtersControl);
+  });
+
+  $(document).ready(function() {
+    $(window).on('resize', _.debounce(function() {
+      if (filtersActive === true) {
+        closeFilters(filtersControl);
+      }
+    }, 250));
+    limitFilters();
+  });
+
+
+})();
---
+++ aws/23-11-2015/js/scroll-lock.js
@@ -0,0 +1,39 @@
+// prevent scroll in container from propagating out to the doc body when we scrol too far
+// to be checked for x browser and toggled for mob
+//
+// TODO - check this is needed - Ian/
+
+// disabled for demo of ui kit on 23/11/15
+
+// $(document).on('DOMMouseScroll mousewheel', '.js-scroll-hold', function(ev) {
+//     console.log(this);
+
+//     var $this = $(this),
+//         scrollTop = this.scrollTop,
+//         scrollHeight = this.scrollHeight,
+//         height = $this.height(),
+//         delta = (ev.type == 'DOMMouseScroll' ?
+//             ev.originalEvent.detail * -40 :
+//             ev.originalEvent.wheelDelta),
+//         up = delta > 0;
+
+//     var prevent = function() {
+//         ev.stopPropagation();
+//         ev.preventDefault();
+//         ev.returnValue = false;
+//         return false;
+//     }
+
+//     // if (window.matchMedia("(min-width: 1024px)").matches) {
+//       if (!up && -delta > scrollHeight - height - scrollTop) {
+//           // Scrolling down, but this will take us past the bottom.
+//           $this.scrollTop(scrollHeight);
+//           return prevent();
+//       } else if (up && delta > scrollTop) {
+//           // Scrolling up, but this will take us past the top.
+//           $this.scrollTop(0);
+//           return prevent();
+//       }
+//     // }
+// });
+
