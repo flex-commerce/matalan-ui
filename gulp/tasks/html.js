a9674f7121dad86248e64989761c29b4f7137e14
Ian Harvey <mail.iharvey@gmail.com>
2015-11-24 18:16:59 +0000

    further workflow tweaks

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,71 +1,43 @@
 /*jslint node: true */
 'use strict';
 
+var config       = require('../config');
+  if(!config.tasks.html) return;
+
 var gulp         = require('gulp'),
-    config       = require('../config'),
     foreach      = require('gulp-foreach'),
     hb           = require('gulp-hb'),
     path         = require('path'),
     prettify     = require('gulp-jsbeautifier'),
     rename       = require('gulp-rename'),
     handleErrors = require('../util/handleErrors'),
-    browserSync  = require('browser-sync');
+    browserSync  = require('browser-sync'),
+    changed     = require('gulp-changed'),
+    gulpif       = require('gulp-if'),
+    htmlmin      = require('gulp-htmlmin');
 
+var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**');
 
+var paths = {
+  src: [path.join(config.app.src, config.tasks.html.src, '/**/*'), exclude],
+  dest: path.join(config.app.dest, config.tasks.html.dest),
+}
 
-gulp.task('html', function () {
-    return gulp
-        .src('./src/htdocs/**/*.html')
-        .pipe(hb({
-            helpers: 'src/hb-helpers/*.js',
-            partials: 'src/htdocs/**/*.hbs',
-            bustCache: true
-        }))
-        .on('error', handleErrors)
-        // .pipe(prettify({config: '.jsbeautifyrc'}))
-        .pipe(gulp.dest(config.app.dest))
-        .pipe(browserSync.reload({stream:true}));
-});
+var htmlTask = function() {
 
-// todo - rebuild this wth handlebars
+  return gulp.src(paths.src)
+    .pipe(hb({
+        helpers: 'src/hb-helpers/*.js',
+        partials: 'src/htdocs/**/*.hbs',
+        bustCache: true
+    }))
+    .on('error', handleErrors)
+    .pipe(gulpif(process.env.NODE_ENV == 'production', htmlmin(config.tasks.html.htmlmin)))
+    .pipe(changed(paths.dest)) // Ignore unchanged files
+    // .pipe(prettify({config: '.jsbeautifyrc'}))
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream());
+}
 
-// var config       = require('../config')
-// if(!config.tasks.html) return
-
-// var browserSync  = require('browser-sync')
-// // var data         = require('gulp-data')
-// var gulp         = require('gulp')
-// var gulpif       = require('gulp-if')
-// var handleErrors = require('../util/handleErrors')
-// var htmlmin      = require('gulp-htmlmin')
-// var path         = require('path')
-// // var render       = require('gulp-nunjucks-render')
-// var fs           = require('fs')
-
-// var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**')
-
-// var paths = {
-//   src: [path.join(config.app.src, config.tasks.html.src, '/**/*.html'), exclude],
-//   dest: path.join(config.app.dest, config.tasks.html.dest),
-// }
-
-// var getData = function(file) {
-//   var dataPath = path.resolve(config.app.src, config.tasks.html.src, config.tasks.html.dataFile)
-//   return JSON.parse(fs.readFileSync(dataPath, 'utf8'))
-// }
-
-// var htmlTask = function() {
-//   // render.nunjucks.configure([path.join(config.app.src, config.tasks.html.src)], {watch: false })
-
-//   return gulp.src(paths.src)
-//     // .pipe(data(getData))
-//     .on('error', handleErrors)
-//     .pipe(render())
-//     .on('error', handleErrors)
-//     .pipe(gulpif(process.env.NODE_ENV == 'production', htmlmin(config.tasks.html.htmlmin)))
-//     .pipe(gulp.dest(paths.dest))
-//     .pipe(browserSync.stream())
-// }
-
-// gulp.task('html', htmlTask)
-// module.exports = htmlTask
+gulp.task('html', htmlTask);
+module.exports = htmlTask;
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,24 +1,12 @@
 'use strict';
 
-// var gulp     = require('gulp');
-// var config   = require('../config');
-
-// gulp.task('watch', ['browserSync'], function(callback) {
-//   gulp.watch(config.sass.src,   ['sass','sass:lint']);
-//   gulp.watch(config.markup.src, ['markup']);
-//   gulp.watch(config.fonts.src,  ['fonts']);
-//   gulp.watch(config.img.src,  ['images']);
-//   gulp.watch(config.js.src,  ['js']);
-// });
-
-
 var config = require('../config');
 var gulp   = require('gulp');
 var path   = require('path');
 var watch  = require('gulp-watch');
 
 var watchTask = function() {
-  var watchableTasks = ['fonts', 'iconFont', 'images', 'svgSprite','html', 'css'];
+  var watchableTasks = ['fonts', 'iconFont', 'images', 'svgSprite', 'html', 'css'];
 
   watchableTasks.forEach(function(taskName) {
     var task = config.tasks[taskName];
