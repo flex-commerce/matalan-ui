713c2a841e349c8457f337abc3762265d66e6c12
Ian Harvey <mail.iharvey@gmail.com>
2015-12-03 15:27:03 +0000

    developer debug tools added

--- src/js/modules/general.js
+++ src/js/modules/general.js
@@ -36,6 +36,20 @@
     $('.o-filters--inner').find('input[type=radio]:checked').removeAttr('checked');
   });
 
+  // ==========================
+  $('#dev-debug').on('click', function() {
+    $(this).parent('li').toggleClass('active');
+    $('html').toggleClass('dev-debug');
+  })
+  $('#grid-debug').on('click', function() {
+    $(this).parent('li').toggleClass('active');
+    $('html').toggleClass('grid-debug');
+  })
+  $('#nudge-debug').on('click', function() {
+    $(this).parent('li').toggleClass('active');
+    $('html').toggleClass('nudge-debug');
+  })
+
 
 
 })(jQuery);
