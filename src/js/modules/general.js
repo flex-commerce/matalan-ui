4846e5313c2785ee7d66e3eb2ad09a06e0e6451e
amjgbr <amjgbr@gmail.com>
2015-12-08 19:08:24 +0000

    javascript for add remove items from bag
    Signed-off-by: amjgbr <amjgbr@gmail.com>

--- src/js/modules/general.js
+++ src/js/modules/general.js
@@ -52,6 +52,32 @@
     $('html').toggleClass('nudge-debug');
   });
 
+  $('.add-item').on('click', function() {
+    var valueBox = $(this).parent().find('span'),
+        hiddenInput = $(this).parent().find('input'),
+        value = parseInt(valueBox.html(),10),
+        maxValue = 10,
+        newValue = value + 1;
+
+    if (newValue <= maxValue) {
+      valueBox.html(newValue);
+      hiddenInput.val(newValue);
+    }
+  });
+
+  $('.remove-item').on('click', function() {
+    var valueBox = $(this).parent().find('span'),
+        hiddenInput = $(this).parent().find('input'),
+        value     = parseInt(valueBox.html(),10),
+        minValue  = 1,
+        newValue  = value - 1;
+
+    if (newValue >= minValue) {
+      valueBox.html(newValue);
+      hiddenInput.val(newValue);
+    }
+  });
+
   // // Cache selectors outside callback for performance.
   // if ($('#searchHeader').length) {
   //   var $window = $(window),
