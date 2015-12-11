e27ec13c0a48fa5b015e15e2d55a756cf027f8b1
Ian Harvey <mail.iharvey@gmail.com>
2015-12-11 13:27:18 +0000

    Fixes id'd during Fridays meeting

--- src/js/modules/off-canvas.js
+++ src/js/modules/off-canvas.js
@@ -69,13 +69,19 @@
 
   // ===========================
   // off canvas button event handler
-  $(".js-nav-mobile-button").on('mousedown', function() {
+  $("body").on('mousedown', '.js-nav-mobile-button',  function() {
     if ($('html').hasClass('body--modal-open')) {
       closeOffCanvas();
     } else {
       openOffCanvas();
     }
   })
+
+    $('body').on('click touchstart', '#js-clickcatcher-canvas', function() {
+      closeOffCanvas();
+    });
+
+
 
   // DEVELOPER RULE - stop links from acting on click
   $('nav a').on('click', function(e) {
