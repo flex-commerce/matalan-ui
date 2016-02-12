b1c576aada00a8f55e37f856633bd3cfdce57ee7
amjgbr <amjgbr@gmail.com>
2016-02-12 21:26:20 +0000

    account details and google map temp work
    Signed-off-by: amjgbr <amjgbr@gmail.com>

--- src/js/modules/general.js
+++ src/js/modules/general.js
@@ -1,157 +1,153 @@
-// (function($) {
-//   'use strict';
+(function($) {
+    'use strict';
+    // var GMaps = require("../vendors/gmaps");
 
-//   function validateEmail(email) {
-//     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-//     return re.test(email);
-//   }
+    // var url = GMaps.staticMapURL({
+    //     lat: 53.796631,
+    //     lng: -1.4698,
+    //     markers: {
+    //       icon : '../../img/marker.svg'
+    //     }
+    // });
 
-//   $('body').on('click', '.close', function() {
-//     var parentId = $(this).data('close');
-//     $('#' + parentId).hide();
-//   });
+    // $('<img/>').attr('src', url)
+    //     .appendTo('#storemap');
 
-//   $('body').on('change', '.image-position', function() {
-//     var sectionId = $(this).closest('select').attr('data-app');
-//     var optionValue = $(this).val();
-//     console.log(optionValue);
-//     $('#' + sectionId).css('background-position', optionValue);
-//   });
+    //   function validateEmail(email) {
+    //     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    //     return re.test(email);
+    //   }
 
-//   $('body').on('click', '#checkoutCreateAccount', function() {
-//     $('.create-account-container').toggle();
+    //   $('body').on('click', '.close', function() {
+    //     var parentId = $(this).data('close');
+    //     $('#' + parentId).hide();
+    //   });
 
-//     window.onbeforeunload = function() {
-//       return "Are you sure you don't want to create an account?";
-//     };
-//   });
+    //   $('body').on('change', '.image-position', function() {
+    //     var sectionId = $(this).closest('select').attr('data-app');
+    //     var optionValue = $(this).val();
+    //     console.log(optionValue);
+    //     $('#' + sectionId).css('background-position', optionValue);
+    //   });
 
-//   $(".zoom").bind("click", function(e) {
-//     var ez = $('.zoom').data('elevateZoom');
-//     $.fancybox(ez.getGalleryList());
-//     return false;
-//   });
+    //   $('body').on('click', '#checkoutCreateAccount', function() {
+    //     $('.create-account-container').toggle();
 
-//   $("#termsAndConditionsAccepted").on("click", function(e) {
-//     if ($(this).is(':checked')) {
-//       $('.iFrameWrapper').show();
-//     } else {
-//       $('.iFrameWrapper').hide();
-//     }
-//   });
+    //     window.onbeforeunload = function() {
+    //       return "Are you sure you don't want to create an account?";
+    //     };
+    //   });
 
-//   $('form#email-signup').submit(function(e) {
-//     e.preventDefault();
-//     $(this).hide();
-//     $('.signup-success').show();
-//   });
+    //   $(".zoom").bind("click", function(e) {
+    //     var ez = $('.zoom').data('elevateZoom');
+    //     $.fancybox(ez.getGalleryList());
+    //     return false;
+    //   });
 
-//   $('#addressEntry').keypress(function(e) {
-//     if (e.which == 13) {
-//       e.preventDefault();
-//       $(this).parent().find('a').click();
-//     }
-//   });
+    //   $("#termsAndConditionsAccepted").on("click", function(e) {
+    //     if ($(this).is(':checked')) {
+    //       $('.iFrameWrapper').show();
+    //     } else {
+    //       $('.iFrameWrapper').hide();
+    //     }
+    //   });
 
+    //   $('form#email-signup').submit(function(e) {
+    //     e.preventDefault();
+    //     $(this).hide();
+    //     $('.signup-success').show();
+    //   });
 
-//   $('input[name=billingaddress]').on('change', function() {
-//     $('.address-picker').toggle();
-//   });
-
-//   $('input[name=deliveryaddress]').on('change', function() {
-
-//     console.log($(this).attr("value"));
-
-//     if ($(this).attr("value") === "addressbook") {
-//       $(".o-box").not(".o-addressbook").hide();
-//       $(".o-addressbook").show();
-//       $(".o-submit-button").show();
-//     } else if ($(this).attr("value") === "newaddress") {
-//       $(".o-box").not(".o-newaddress").hide();
-//       $(".o-newaddress").show();
-//     } else {
-//       $(".o-box").not(".o-submit-button").hide();
-//       $(".o-submit-button").show();
-//     }
-//   });
-
-//   $('input[name=deliverytype]').on('change', function() {
-//     if ($(this).attr("value") === "click-and-collect") {
-//       $(".o-box").not(".o-click-and-collect").hide();
-//       $(".o-click-and-collect").show();
-//     } else {
-//       $(".o-box").not(".o-submit-button").hide();
-//       $(".o-submit-button").show();
-//     }
-//   });
+    //   $('#addressEntry').keypress(function(e) {
+    //     if (e.which == 13) {
+    //       e.preventDefault();
+    //       $(this).parent().find('a').click();
+    //     }
+    //   });
 
 
+    //   $('input[name=billingaddress]').on('change', function() {
+    //     $('.address-picker').toggle();
+    //   });
 
-//   $('.editAddress').on('click', function() {
-//     $('.address-picker').toggle();
-//     $("#newaddress").prop('checked', true);
-//   });
+    //   $('input[name=deliveryaddress]').on('change', function() {
 
-//   $('.is-dev .o-login__email-submit').on('click', function(e) {
-//     e.preventDefault();
-//     $('.o-login__email').hide();
-//     $('.o-login__password').show();
-//   });
+    //     console.log($(this).attr("value"));
 
-//   $('.is-dev .o-login__password-forgotten').on('click', function(e) {
-//     e.preventDefault();
-//     $('.o-login__password').hide();
-//     $('.o-login__forgotten-password').show();
-//   });
+    //     if ($(this).attr("value") === "addressbook") {
+    //       $(".o-box").not(".o-addressbook").hide();
+    //       $(".o-addressbook").show();
+    //       $(".o-submit-button").show();
+    //     } else if ($(this).attr("value") === "newaddress") {
+    //       $(".o-box").not(".o-newaddress").hide();
+    //       $(".o-newaddress").show();
+    //     } else {
+    //       $(".o-box").not(".o-submit-button").hide();
+    //       $(".o-submit-button").show();
+    //     }
+    //   });
 
-//   $('form[name=searchProducts]').submit(function(e) {
-//     e.preventDefault();
-//     // console.log($('input[name=searchTermMobile]').val());
-//     // console.log($('input[name=searchTerm]').val());
-//     if ($('input[name=searchTerm]').val() == 'dresses' || $('input[name=searchTermMobile]').val() == 'dresses') {
-//       window.location.href = './search-results.html';
-//     } else {
-//       window.location.href = './no-results.html';
-//     }
-//   });
-
-//   $('form#createAccountForm').submit(function(e) {
-//     e.preventDefault();
-//     // console.log($('input[name=searchTermMobile]').val());
-//     // console.log($('input[name=searchTerm]').val());
-//     $('.create-account-container').hide();
-//     $('.account-created-container').show();
-//     window.onbeforeunload = function() {
-//       // blank function do nothing
-//     }
-//   });
+    //   $('input[name=deliverytype]').on('change', function() {
+    //     if ($(this).attr("value") === "click-and-collect") {
+    //       $(".o-box").not(".o-click-and-collect").hide();
+    //       $(".o-click-and-collect").show();
+    //     } else {
+    //       $(".o-box").not(".o-submit-button").hide();
+    //       $(".o-submit-button").show();
+    //     }
+    //   });
 
 
 
+    //   $('.editAddress').on('click', function() {
+    //     $('.address-picker').toggle();
+    //     $("#newaddress").prop('checked', true);
+    //   });
 
+    //   $('.is-dev .o-login__email-submit').on('click', function(e) {
+    //     e.preventDefault();
+    //     $('.o-login__email').hide();
+    //     $('.o-login__password').show();
+    //   });
 
-//   $('body').on('click', '.js-filters__clear', function() {
-//     $('.o-filters__inner').find('input[type=checkbox]:checked').removeAttr('checked');
-//     $('.o-filters__inner').find('input[type=radio]:checked').removeAttr('checked');
-//   });
+    //   $('.is-dev .o-login__password-forgotten').on('click', function(e) {
+    //     e.preventDefault();
+    //     $('.o-login__password').hide();
+    //     $('.o-login__forgotten-password').show();
+    //   });
 
-//   // ==========================
+    //   $('form[name=searchProducts]').submit(function(e) {
+    //     e.preventDefault();
+    //     // console.log($('input[name=searchTermMobile]').val());
+    //     // console.log($('input[name=searchTerm]').val());
+    //     if ($('input[name=searchTerm]').val() == 'dresses' || $('input[name=searchTermMobile]').val() == 'dresses') {
+    //       window.location.href = './search-results.html';
+    //     } else {
+    //       window.location.href = './no-results.html';
+    //     }
+    //   });
 
-
-
+    //   $('form#createAccountForm').submit(function(e) {
+    //     e.preventDefault();
+    //     // console.log($('input[name=searchTermMobile]').val());
+    //     // console.log($('input[name=searchTerm]').val());
+    //     $('.create-account-container').hide();
+    //     $('.account-created-container').show();
+    //     window.onbeforeunload = function() {
+    //       // blank function do nothing
+    //     }
+    //   });
 
 
 
 
-//   $('.filter--colours li').click(function(e) {
-//     $(this).toggleClass('active');
-//     $(this).siblings().removeClass('active');
-//   });
 
+    //   $('body').on('click', '.js-filters__clear', function() {
+    //     $('.o-filters__inner').find('input[type=checkbox]:checked').removeAttr('checked');
+    //     $('.o-filters__inner').find('input[type=radio]:checked').removeAttr('checked');
+    //   });
 
-
-
-//   // ==========================
+    //   // ==========================
 
 
 
@@ -159,19 +155,35 @@
 
 
 
+    //   $('.filter--colours li').click(function(e) {
+    //     $(this).toggleClass('active');
+    //     $(this).siblings().removeClass('active');
+    //   });
 
 
 
 
-//   // // Cache selectors outside callback for performance.
-//   // if ($('#searchHeader').length) {
-//   //   var $window = $(window),
-//   //      $stickyEl = $('#searchHeader'),
-//   //      elTop = $stickyEl.offset().top;
+    //   // ==========================
 
-//   //   $window.scroll(function() {
-//   //       $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-//   //   });
-//   // }
 
-// })(jQuery);
+
+
+
+
+
+
+
+
+
+    //   // // Cache selectors outside callback for performance.
+    //   // if ($('#searchHeader').length) {
+    //   //   var $window = $(window),
+    //   //      $stickyEl = $('#searchHeader'),
+    //   //      elTop = $stickyEl.offset().top;
+
+    //   //   $window.scroll(function() {
+    //   //       $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+    //   //   });
+    //   // }
+
+})(jQuery);
