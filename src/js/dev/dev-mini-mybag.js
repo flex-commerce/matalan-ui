dc8ea4840b76cc8f5d242cfc1d69a3ba6e0bc71e
amjgbr <amjgbr@gmail.com>
2016-03-04 08:41:46 +0000

    safar slider fix test
    Signed-off-by: amjgbr <amjgbr@gmail.com>

--- src/js/dev/dev-mini-mybag.js
+++ src/js/dev/dev-mini-mybag.js
@@ -1,32 +1,32 @@
 // Designed for the mobile minibag, we can make this more agnostic
 // or provide the expected markup here
 
-$('.js-add-item').on('click', function() {
-  var valueBox = $(this).parent().find('span'),
-    hiddenInput = $(this).parent().find('input'),
-    value = parseInt(valueBox.html(), 10),
-    maxValue = 10,
-    newValue = value + 1;
+// $('.js-add-item').on('click', function() {
+//   var valueBox = $(this).parent().find('span'),
+//     hiddenInput = $(this).parent().find('input'),
+//     value = parseInt(valueBox.html(), 10),
+//     maxValue = 10,
+//     newValue = value + 1;
 
-  if (newValue <= maxValue) {
-    valueBox.html(newValue);
-    hiddenInput.val(newValue);
-  }
-});
+//   if (newValue <= maxValue) {
+//     valueBox.html(newValue);
+//     hiddenInput.val(newValue);
+//   }
+// });
 
 
-$('.js-remove-item').on('click', function() {
-  var valueBox = $(this).parent().find('span'),
-    hiddenInput = $(this).parent().find('input'),
-    value = parseInt(valueBox.html(), 10),
-    minValue = 1,
-    newValue = value - 1;
+// $('.js-remove-item').on('click', function() {
+//   var valueBox = $(this).parent().find('span'),
+//     hiddenInput = $(this).parent().find('input'),
+//     value = parseInt(valueBox.html(), 10),
+//     minValue = 1,
+//     newValue = value - 1;
 
-  if (newValue >= minValue) {
-    valueBox.html(newValue);
-    hiddenInput.val(newValue);
-  }
-});
+//   if (newValue >= minValue) {
+//     valueBox.html(newValue);
+//     hiddenInput.val(newValue);
+//   }
+// });
 
 
 
